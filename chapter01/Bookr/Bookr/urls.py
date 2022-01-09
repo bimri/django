e39c793acd14b3aa46ef088fb4684e8562b0c2bf
bimri <bimri@outlook.com>
2022-01-09 18:28:11 +0300

    Exercise 1.03: Writing a View and Mapping a URL to It

--- chapter01/Bookr/Bookr/urls.py
+++ chapter01/Bookr/Bookr/urls.py
@@ -16,6 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+import reviews.views
+
+urlpatterns = [path("admin/", admin.site.urls), path("", reviews.views.index)]
