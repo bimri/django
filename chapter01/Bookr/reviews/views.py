e2542e9b5beaaff289cc41d761b16d9073b217a4
bimri <bimri@outlook.com>
2022-01-09 20:21:36 +0300

    Rendering Variables in Templates

--- chapter01/Bookr/reviews/views.py
+++ chapter01/Bookr/reviews/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "base.html")
- 
+    name = "world"
+    return render(request, "base.html", {'name': name})
+   
