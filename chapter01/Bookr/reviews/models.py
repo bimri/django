7ca7aee7e5bf3f095be7516a5d4eeb72549394b9
bimri <bimri@outlook.com>
2022-01-12 23:53:15 +0300

    Adding the Review Model

--- chapter01/Bookr/reviews/models.py
+++ chapter01/Bookr/reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import auth
 
 
 class Publisher(models.Model):
@@ -57,3 +58,18 @@ class BookContributor(models.Model):
 # class DriverLicence(models.Model):
 #     person = models.OneToOneField(Person, on_delete=models.CASCADE)
 #     licence_number = models.CharField(max_length=50)
+
+
+class Review(models.Model):
+    content = models.TextField(help_text="The Review text.")
+    rating = models.IntegerField(help_text="The rating the reviewer has given.")
+    date_created = models.DateTimeField(
+        auto_now_add=True, help_text="The date and time the review was created."
+    )
+    date_edited = models.DateTimeField(
+        null=True, help_text="The date and time the review was last edited."
+    )
+    creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, help_text="The Book that this review is for."
+    )
