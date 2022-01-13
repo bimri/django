332a0d0838fc0fd7b14c43943936903c4a0471b3
bimri <bimri@outlook.com>
2022-01-13 22:44:33 +0300

    Juggler models & app settings

--- chapter02/Juggler/projectp/models.py
+++ chapter02/Juggler/projectp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=40, help_text="Project title")
+    creation_time = models.DateTimeField(auto_now_add=True, help_text="Start time")
+    completion_time = models.DateTimeField(null=True, help_text="End time")
+
+    def __str__(self):
+        return self.title
+
+
+class Task(models.Model):
+    """Activity to be completed"""
+
+    activity = models.CharField(max_length=40, help_text="Title the activity")
+    description = models.TextField(help_text="More information on activity to partake")
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    time_estimate = models.IntegerField(
+        help_text="Time in hours required to compelete activity"
+    )
+    completed = models.BooleanField(default=False, help_text="Task completion status")
+
+    def __str__(self):
+        return self.activity
