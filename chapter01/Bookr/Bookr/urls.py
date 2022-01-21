09a5202c6779253ce335338b756f06fc8b72f600
bimri <bimri@outlook.com>
2022-01-22 00:08:38 +0300

    views exercises

--- chapter01/Bookr/Bookr/urls.py
+++ chapter01/Bookr/Bookr/urls.py
@@ -21,5 +21,5 @@ import reviews.views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("book-search/", reviews.views.book_search),
-    path("", include('reviews.urls'))
+    path("", include("reviews.urls")),
 ]
