e26a10b1921132cd35224a79ca73ad336a9cf38e
bimri <bimri@outlook.com>
2022-01-23 19:11:41 +0300

    Registering the Reviews Model

--- chapter01/Bookr/reviews/admin.py
+++ chapter01/Bookr/reviews/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from reviews.models import Publisher, Review, Contributor, Book, BookContributor
+
+
 # Register your models here.
+admin.site.register(Publisher)
+admin.site.register(Review)
+admin.site.register(Contributor)
+admin.site.register(Book)
+admin.site.register(BookContributor)
