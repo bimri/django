e2696ed8510e705696cb2b5c37fbf978aa826c61
bimri <bimri@outlook.com>
2022-01-23 20:26:22 +0300

    Subclassing AdminSite

--- chapter01/Bookr/Bookr/urls.py
+++ chapter01/Bookr/Bookr/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin_site
 from django.urls import include, path
 
 import reviews.views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", admin_site.site.urls),
     path("book-search/", reviews.views.book_search),
     path("", include("reviews.urls")),
     path("books/", include("reviews.urls")),
--- chapter01/Bookr/reviews/admin.py
+++ chapter01/Bookr/reviews/admin.py
@@ -1,11 +1,20 @@
-from django.contrib import admin
+from django.contrib import AdminSite
 
 from reviews.models import Publisher, Review, Contributor, Book, BookContributor
 
 
+class BookrAdminSite(AdminSite):
+    title_header = "Bookr Admin"
+    site_header = "Bookr Administration"
+    index_title = "Bookr site admin"
+
+
+admin_site = BookrAdminSite(name="bookr")
+
+
 # Register your models here.
-admin.site.register(Publisher)
-admin.site.register(Review)
-admin.site.register(Contributor)
-admin.site.register(Book)
-admin.site.register(BookContributor)
+admin_site.site.register(Publisher)
+admin_site.site.register(Review)
+admin_site.site.register(Contributor)
+admin_site.site.register(Book)
+admin_site.site.register(BookContributor)
