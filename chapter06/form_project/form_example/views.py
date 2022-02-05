63a2548d4986e7c24fe1e64e419e7532bbf69fd8
bimri <bimri@outlook.com>
2022-02-05 20:10:28 +0300

    Exercise 6.03: Building and Rendering a Django Form

--- chapter06/form_project/form_example/views.py
+++ chapter06/form_project/form_example/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .forms import ExampleForm
 
 
 def form_example(request):
+    form = ExampleForm()
+
     for name in request.POST:
         print("{}: {}".format(name, request.POST.getlist(name)))
 
-    return render(request, "form-example.html", {"method": request.method})
+    return render(
+        request, "form-example.html", {"method": request.method, "form": form}
+    )
