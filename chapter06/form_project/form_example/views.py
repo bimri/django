6356b5ee1cfce155c96718eee8dddba063822c76
bimri <bimri@outlook.com>
2022-02-05 17:35:57 +0300

    Exercise 6.02: Working with POST Data in a View

--- chapter06/form_project/form_example/views.py
+++ chapter06/form_project/form_example/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
+
 def form_example(request):
-    return render(request, "form-example.html")
+    for name in request.POST:
+        print("{}: {}".format(name, request.POST.getlist(name)))
+
+    return render(request, "form-example.html", {"method": request.method})
