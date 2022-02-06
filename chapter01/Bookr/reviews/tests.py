e49b6936aab1d737a871c577fddfc2715ef7fa2a
bimri <bimri@outlook.com>
2022-02-06 19:05:41 +0300

    Activity 7.01: Styling and Integrating the Publisher Form

--- chapter01/Bookr/reviews/tests.py
+++ chapter01/Bookr/reviews/tests.py
@@ -6,7 +6,13 @@ from django.test import TestCase
 from reviews.models import Publisher
 
 
-class Exercise3Test(TestCase):
+class Activity1Test(TestCase):
+    def test_container_wrapper(self):
+        """The <div class="container-fluid"> should have been added."""
+        c = Client()
+        resp = c.get('/')
+        self.assertIn(b'<div class="container-fluid">', resp.content)
+
     def test_fields_in_view(self):
         """"
         Test that fields exist in the rendered template.
@@ -15,7 +21,7 @@ class Exercise3Test(TestCase):
         response = c.get('/publishers/new/')
 
         self.assertIsNotNone(re.search(r'<input type="hidden" name="csrfmiddlewaretoken" value="\w+">',
-                                       response.content.decode('ascii')))
+                                       response.content.decode('utf8')))
 
         self.assertIn(
             b'<label for="id_name">Name:</label> <input type="text" name="name" maxlength="50" required id="id_name"> '
@@ -29,7 +35,8 @@ class Exercise3Test(TestCase):
             b'<label for="id_email">Email:</label> <input type="email" name="email" maxlength="254" '
             b'required id="id_email"> <span class="helptext">The Publisher\'s email address.</span>',
             response.content)
-        self.assertIn(b'<input type="submit" value="Submit">', response.content)
+        self.assertIn(b'<button type="submit" class="btn btn-primary">\n        Create\n    </button>',
+                      response.content)
 
     def test_publisher_create(self):
         """Test the creation of a new Publisher"""
@@ -52,6 +59,15 @@ class Exercise3Test(TestCase):
         self.assertEqual(publisher.website, publisher_website)
         self.assertEqual(publisher.email, publisher_email)
         self.assertEqual(response['Location'], '/publishers/{}/'.format(publisher.pk))
+
+        # the messages will be on the redirected to page
+        response = c.get(response['location'])
+
+        condensed_content = re.sub(r'\s+', ' ', response.content.decode('utf8').replace('\n', ''))
+
+        self.assertIn(
+            '<div class="alert alert-success" role="alert"> Publisher &quot;Test Create Publisher&quot; was created. '
+            '</div>', condensed_content)
 
     def test_publisher_no_create(self):
         """Test that no Publisher is created if the form is invalid."""
@@ -86,6 +102,8 @@ class Exercise3Test(TestCase):
         self.assertIn(b'value="Test Edit Publisher"', response.content)
         self.assertIn(b'value="http://www.example.com/edit-publisher/"', response.content)
         self.assertIn(b'value="edit-publisher@example.com"', response.content)
+        self.assertIn(b'<button type="submit" class="btn btn-primary">\n        Save\n    </button>',
+                      response.content)
 
         response = c.post('/publishers/{}/'.format(publisher.pk), {
             'name': 'Updated Name',
@@ -100,3 +118,19 @@ class Exercise3Test(TestCase):
         self.assertEqual(publisher2.name, 'Updated Name')
         self.assertEqual(publisher2.website, 'https://www.example.com/updated/')
         self.assertEqual(publisher2.email, 'updated@example.com')
+
+        # the messages will be on the redirected to page
+
+        response = c.get(response['location'])
+
+        condensed_content = re.sub(r'\s+', ' ', response.content.decode('utf8').replace('\n', ''))
+
+        self.assertIn(
+            '<div class="alert alert-success" role="alert"> Publisher &quot;Updated Name&quot; was updated. </div>',
+            condensed_content)
+
+    def test_404_response(self):
+        """When getting a Publisher that doesn't exist we should get a 404 response."""
+        c = Client()
+        response = c.get('/publishers/123/')
+        self.assertEquals(response.status_code, 404)
