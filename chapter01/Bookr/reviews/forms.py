ce501dce4c5dba974d761d98e65b49e5c27320db
bimri <bimri@outlook.com>
2022-02-06 19:01:03 +0300

    Exercise 7.03: Creating and Editing a Publisher

--- chapter01/Bookr/reviews/forms.py
+++ chapter01/Bookr/reviews/forms.py
@@ -3,10 +3,16 @@ from typing_extensions import Required
 from urllib import request
 from django import forms
 from numpy import require
-
+from .models import Publisher
 
 class SearchForm(forms.Form):
     search = forms.CharField(required=False, min_length=3)
     search_in = forms.ChoiceWidget(
         required=False, choices=(("title", "Title"), ("contributor", "Contributor"))
     )
+
+
+class PubliserForm(forms.Form):
+    class Meta:
+        model = Publisher 
+        fields = "__all__"
--- chapter01/Bookr/reviews/views.py
+++ chapter01/Bookr/reviews/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import SearchForm
-from .models import Book, Contributor
+from .forms import PublisherForm, SearchForm
+from .models import Book, Contributor, Publisher
 from .utils import average_rating
 
 
@@ -21,25 +22,21 @@ def book_search(request):
         if search_in == "title":
             books = Book.objects.filter(title__icontains=search)
         else:
-            fname_contributors = Contributor.objects.filter(
-                first_names__icontains=search 
-            )
+            fname_contributors = \
+                Contributor.objects.filter(first_names__icontains=search)
 
             for contributor in fname_contributors:
                 for book in contributor.book_set.all():
                     books.add(book)
 
-        lname_contributors = Contributor.objects.filter(last_names__icontains=search)
+        lname_contributors = \
+            Contributor.objects.filter(last_names__icontains=search)
 
         for contributor in lname_contributors:
             for book in contributor.book_set.all():
                 books.add(book)
 
-    return render(
-        request,
-        "reviews/search-results.html",
-        {"form": form, "search_text": search_text, "books": books},
-    )
+    return render(request, "reviews/search-results.html", {"form": form, "search_text": search_text, "books": books})
 
 
 def book_list(request):
@@ -53,15 +50,11 @@ def book_list(request):
         else:
             book_rating = None
             number_of_reviews = 0
-        books_with_reviews.append(
-            {
-                "book": book,
-                "book_rating": book_rating,
-                "number_of_reviews": number_of_reviews,
-            }
-        )
+        books_with_reviews.append({"book": book, "book_rating": book_rating, "number_of_reviews": number_of_reviews})
 
-    context = {"book_list": books_with_reviews}
+    context = {
+        "book_list": books_with_reviews
+    }
     return render(request, "reviews/book_list.html", context)
 
 
@@ -70,8 +63,37 @@ def book_detail(request, pk):
     reviews = book.review_set.all()
     if reviews:
         book_rating = average_rating([review.rating for review in reviews])
-        context = {"book": book, "book_rating": book_rating, "reviews": reviews}
+        context = {
+            "book": book,
+            "book_rating": book_rating,
+            "reviews": reviews
+        }
     else:
-        context = {"book": book, "book_rating": None, "reviews": None}
+        context = {
+            "book": book,
+            "book_rating": None,
+            "reviews": None
+        }
     return render(request, "reviews/book_detail.html", context)
-  
+
+
+def publisher_edit(request, pk=None):
+    if pk is not None:
+        publisher = get_object_or_404(Publisher, pk=pk)
+    else:
+        publisher = None
+
+    if request.method == "POST":
+        form = PublisherForm(request.POST, instance=publisher)
+        if form.is_valid():
+            updated_publisher = form.save()
+            if publisher is None:
+                messages.success(request, "Publisher \"{}\" was created.".format(updated_publisher))
+            else:
+                messages.success(request, "Publisher \"{}\" was updated.".format(updated_publisher))
+
+            return redirect("publisher_edit", updated_publisher.pk)
+    else:
+        form = PublisherForm(instance=publisher)
+
+    return render(request, "form-example.html", {"method": request.method, "form": form})
