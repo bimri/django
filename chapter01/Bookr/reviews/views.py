f6addd1605c3304938c6f1985d0f572f97a03f6d
bimri <bimri@outlook.com>
2022-02-08 21:13:01 +0300

    Exercise 9.03: Adding Authentication Decorators to the Views

--- chapter01/Bookr/reviews/views.py
+++ chapter01/Bookr/reviews/views.py
@@ -2,6 +2,8 @@ from io import BytesIO
 
 from PIL import Image
 from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test, permission_required, login_required
+from django.core.exceptions import PermissionDenied
 from django.core.files.images import ImageFile
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
@@ -25,25 +27,21 @@ def book_search(request):
         if search_in == "title":
             books = Book.objects.filter(title__icontains=search)
         else:
-            fname_contributors = Contributor.objects.filter(
-                first_names__icontains=search
-            )
+            fname_contributors = \
+                Contributor.objects.filter(first_names__icontains=search)
 
             for contributor in fname_contributors:
                 for book in contributor.book_set.all():
                     books.add(book)
 
-        lname_contributors = Contributor.objects.filter(last_names__icontains=search)
+        lname_contributors = \
+            Contributor.objects.filter(last_names__icontains=search)
 
         for contributor in lname_contributors:
             for book in contributor.book_set.all():
                 books.add(book)
 
-    return render(
-        request,
-        "reviews/search-results.html",
-        {"form": form, "search_text": search_text, "books": books},
-    )
+    return render(request, "reviews/search-results.html", {"form": form, "search_text": search_text, "books": books})
 
 
 def book_list(request):
@@ -57,15 +55,11 @@ def book_list(request):
         else:
             book_rating = None
             number_of_reviews = 0
-        books_with_reviews.append(
-            {
-                "book": book,
-                "book_rating": book_rating,
-                "number_of_reviews": number_of_reviews,
-            }
-        )
+        books_with_reviews.append({"book": book, "book_rating": book_rating, "number_of_reviews": number_of_reviews})
 
-    context = {"book_list": books_with_reviews}
+    context = {
+        "book_list": books_with_reviews
+    }
     return render(request, "reviews/book_list.html", context)
 
 
@@ -74,12 +68,27 @@ def book_detail(request, pk):
     reviews = book.review_set.all()
     if reviews:
         book_rating = average_rating([review.rating for review in reviews])
-        context = {"book": book, "book_rating": book_rating, "reviews": reviews}
+        context = {
+            "book": book,
+            "book_rating": book_rating,
+            "reviews": reviews
+        }
     else:
-        context = {"book": book, "book_rating": None, "reviews": None}
+        context = {
+            "book": book,
+            "book_rating": None,
+            "reviews": None
+        }
     return render(request, "reviews/book_detail.html", context)
 
 
+# 9.03.2
+def is_staff_user(user):
+    return user.is_staff
+
+
+# @permission_required('edit_publishers')  # 9.03.1
+@user_passes_test(is_staff_user)  # 9.03.2
 def publisher_edit(request, pk=None):
     if pk is not None:
         publisher = get_object_or_404(Publisher, pk=pk)
@@ -91,30 +100,27 @@ def publisher_edit(request, pk=None):
         if form.is_valid():
             updated_publisher = form.save()
             if publisher is None:
-                messages.success(
-                    request, 'Publisher "{}" was created.'.format(updated_publisher)
-                )
+                messages.success(request, "Publisher \"{}\" was created.".format(updated_publisher))
             else:
-                messages.success(
-                    request, 'Publisher "{}" was updated.'.format(updated_publisher)
-                )
+                messages.success(request, "Publisher \"{}\" was updated.".format(updated_publisher))
 
             return redirect("publisher_edit", updated_publisher.pk)
     else:
         form = PublisherForm(instance=publisher)
 
-    return render(
-        request,
-        "reviews/instance-form.html",
-        {"form": form, "instance": publisher, "model_type": "Publisher"},
-    )
+    return render(request, "reviews/instance-form.html",
+                  {"form": form, "instance": publisher, "model_type": "Publisher"})
 
 
+@login_required  # 9.03.3
 def review_edit(request, book_pk, review_pk=None):
     book = get_object_or_404(Book, pk=book_pk)
 
     if review_pk is not None:
         review = get_object_or_404(Review, book_id=book_pk, pk=review_pk)
+        user = request.user
+        if not user.is_staff and review.creator.id != user.id:
+            raise PermissionDenied
     else:
         review = None
 
@@ -126,29 +132,26 @@ def review_edit(request, book_pk, review_pk=None):
             updated_review.book = book
 
             if review is None:
-                messages.success(request, 'Review for "{}" created.'.format(book))
+                messages.success(request, "Review for \"{}\" created.".format(book))
             else:
                 updated_review.date_edited = timezone.now()
-                messages.success(request, 'Review for "{}" updated.'.format(book))
+                messages.success(request, "Review for \"{}\" updated.".format(book))
 
             updated_review.save()
             return redirect("book_detail", book.pk)
     else:
         form = ReviewForm(instance=review)
 
-    return render(
-        request,
-        "reviews/instance-form.html",
-        {
-            "form": form,
-            "instance": review,
-            "model_type": "Review",
-            "related_instance": book,
-            "related_model_type": "Book",
-        },
-    )
+    return render(request, "reviews/instance-form.html",
+                  {"form": form,
+                   "instance": review,
+                   "model_type": "Review",
+                   "related_instance": book,
+                   "related_model_type": "Book"
+                   })
 
 
+@login_required  # 9.03.3
 def book_media(request, pk):
     book = get_object_or_404(Book, pk=pk)
 
@@ -168,15 +171,10 @@ def book_media(request, pk):
                 image_file = ImageFile(image_data)
                 book.cover.save(cover.name, image_file)
             book.save()
-            messages.success(
-                request, 'Book "{}" was successfully updated.'.format(book)
-            )
+            messages.success(request, "Book \"{}\" was successfully updated.".format(book))
             return redirect("book_detail", book.pk)
     else:
         form = BookMediaForm(instance=book)
 
-    return render(
-        request,
-        "reviews/instance-form.html",
-        {"instance": book, "form": form, "model_type": "Book", "is_file_upload": True},
-    )
+    return render(request, "reviews/instance-form.html",
+                  {"instance": book, "form": form, "model_type": "Book", "is_file_upload": True})
