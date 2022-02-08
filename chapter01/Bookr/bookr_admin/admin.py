26eda0f5b629c054c5f2ee6dc82cdfdda88c7194
bimri <bimri@outlook.com>
2022-02-09 01:18:04 +0300

    override attributes in custom admin site

--- chapter01/Bookr/bookr_admin/admin.py
+++ chapter01/Bookr/bookr_admin/admin.py
@@ -1,6 +1,9 @@
+from operator import index
 from django.contrib import admin
 from django.contrib.admin.apps import AdminConfig
 
 class BookrAdmin(AdminConfig):
     default_site = 'bookr_admin.admin.BookrAdmin'
     site_header = "Bookr Administration"
+    site_title = "Bookr"
+    index_title = "Administration Panel"
