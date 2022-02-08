2ecad21868cab806d919a22f06764b66dfcaa3f1
bimri <bimri@outlook.com>
2022-02-08 04:58:13 +0300

    Activity 8.02: Displaying Cover and Sample Links

--- chapter01/Bookr/reviews/tests.py
+++ chapter01/Bookr/reviews/tests.py
@@ -1,64 +1,22 @@
 import os
-from unittest import mock
 
-from PIL import Image
-from django import forms
 from django.conf import settings
 from django.test import TestCase, Client
 from django.utils import timezone
 
-from reviews.forms import BookMediaForm
 from reviews.models import Book, Publisher
-from reviews.views import book_media
 
 
-class Activity1Test(TestCase):
+class Activity2Test(TestCase):
     @classmethod
     def setUpTestData(cls):
         p = Publisher.objects.create(name='Test Publisher')
-        b = Book.objects.create(title='Test Book', publication_date=timezone.now(), publisher=p)
+        Book.objects.create(title='Test Book', publication_date=timezone.now(), publisher=p)
 
-    def test_form_definition(self):
-        """Test that the BookMediaForm has the correct field."""
-        f = BookMediaForm()
-        self.assertEquals(f.Meta.model, Book)
-        self.assertEquals(f.Meta.fields, ['cover', 'sample'])
-        self.assertIsInstance(f.fields['cover'], forms.ImageField)
-        self.assertIsInstance(f.fields['sample'], forms.FileField)
-
-    def test_form_in_template(self):
-        """Test that the form is in the rendered template."""
-        c = Client()
-        resp = c.get('/books/1/media/')
-        self.assertEquals(resp.status_code, 200)
-        self.assertIn(b'<label for="id_sample">Sample:</label>', resp.content)
-        self.assertIn(b'<label for="id_cover">Cover:</label>', resp.content)
-        self.assertIn(b'<input type="file" name="cover" accept="image/*" id="id_cover">',
-                      resp.content)
-        self.assertIn(b'<input type="file" name="sample" id="id_sample">',
-                      resp.content)
-
-    @mock.patch('reviews.views.render', name='render')
-    @mock.patch('reviews.views.get_object_or_404', name='get_object_or_404')
-    def test_render_call(self, mock_g_o_o_404 ,mock_render):
-        """Test that the view calls render with the correct arguments and returns it."""
-        request = mock.MagicMock(name='request')
-        resp = book_media(request, 'pk')
-
-        mock_g_o_o_404.assert_called_with(Book, pk='pk')
-        self.assertEquals(resp, mock_render.return_value)
-        self.assertEquals(mock_render.call_args[0][0], request)
-        self.assertEquals(mock_render.call_args[0][1], 'reviews/instance-form.html')
-        self.assertIsInstance(mock_render.call_args[0][2], dict)
-        self.assertEquals(len(mock_render.call_args[0][2]), 4)
-        self.assertIsInstance(mock_render.call_args[0][2]['form'], BookMediaForm)
-        self.assertEquals(mock_render.call_args[0][2]['instance'], mock_g_o_o_404.return_value)
-        self.assertEquals(mock_render.call_args[0][2]['model_type'], 'Book')
-        self.assertEquals(mock_render.call_args[0][2]['is_file_upload'], True)
-
-    def test_book_media_upload(self):
+    def test_book_detail_media_display(self):
         """
-        Test the upload functionality to the book_media view. Check it exists on disk and the image has been resized.
+        When we first view a book we should not see a cover image or link to sample. But if we upload these, they should
+        then be displayed on the book detail page.
         """
         cover_filename = 'machine-learning-for-algorithmic-trading.png'
         cover_save_path = os.path.join(settings.MEDIA_ROOT, 'book_covers', cover_filename)
@@ -66,22 +24,31 @@ class Activity1Test(TestCase):
         sample_filename = 'machine-learning-for-trading.pdf'
         sample_save_path = os.path.join(settings.MEDIA_ROOT, 'book_samples', sample_filename)
 
+        cover_img = b'<img src="/media/book_covers/machine-learning-for-algorithmic-trading.png">'
+        sample_link = b'<a href="/media/book_samples/machine-learning-for-trading.pdf">Download</a>'
+
+        c = Client()
+        resp = c.get('/books/1/')
+
+        self.assertIn(b'<a class="btn btn-primary" href="/books/1/media/">Media</a>', resp.content)
+
+        # check the cover image and sample link aren't in the initial HTML
+        self.assertNotIn(cover_img, resp.content)
+        self.assertNotIn(sample_link, resp.content)
+
         try:
-            c = Client()
             with open(os.path.join(settings.BASE_DIR, 'fixtures', cover_filename), 'rb') as cover_fp:
                 with open(os.path.join(settings.BASE_DIR, 'fixtures', sample_filename), 'rb') as sample_fp:
-                    resp = c.post('/books/1/media/', {'cover': cover_fp, 'sample': sample_fp})
-
-            self.assertEquals(resp.status_code, 302)
-            self.assertEquals(resp['Location'], '/books/1/')
-
-            with open(cover_save_path, 'rb') as cover_image_fp:
-                cover = Image.open(cover_image_fp)
-                self.assertTrue(cover.width == 300 or cover.height == 300)
-
+                    c.post('/books/1/media/', {'cover': cover_fp, 'sample': sample_fp})
         finally:
             if os.path.exists(cover_save_path):
                 os.unlink(cover_save_path)
 
             if os.path.exists(sample_save_path):
                 os.unlink(sample_save_path)
+
+        resp = c.get('/books/1/')
+
+        # check the cover image and sample link are in the HTML after uploading the media
+        self.assertIn(cover_img, resp.content)
+        self.assertIn(sample_link, resp.content)
